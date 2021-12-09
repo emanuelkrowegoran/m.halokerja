5d607500271003134df760918b9b3259ff23f3f3
emanuelkrowegoran <emanuelkrowegoran@gmail.com>
2021-12-09 15:52:07 +0700

    area,pasang iklan, format iklan

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,6 +40,34 @@ $(window).on('load', function() {
         }
     });
 
+     /**
+     * Cara Pasang Iklan Mobile Slider
+     */
+    new Swiper('.pasangiklanmobile-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 40
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 60
+            }
+        }
+    });
+
 
     new Swiper('.sectionlowongan-slider', {
         speed: 400,
